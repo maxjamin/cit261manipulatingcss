312cdd0725a7c077886c7d922ef59a6954a6b810
maxjamin <maxjamin@gmail.com>
2019-01-31 10:11:21 -0700

    one

---
+++ main.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel = "stylesheet"
+   	type = "text/css"
+   	href = "myStyle.css" />
+
+</head>
+
+
+<body>
+
+<h1>BikeRiding</h1>
+
+<div class="navbar">
+  <a href="main.php">Home</a>
+  <a href="pageTwo.php">Fixing</a>
+  <div class="dropdown">
+    <button class="dropbtn">Assignments 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Assignment 1</a>
+      <a href="#">Assignment 2</a>
+      <a href="#">Assignment 3</a>
+    </div>
+  </div> 
+</div>
+<br>
+<img src="bike01.jpeg" alt="Icon" class="picture01">
+
+<p>Hello, and welcome to bikeriders. We have been in business for over 10 years and your one stop place for finding places to get out and ride. Whether your in a city or in the middle of nowhere, we hope that you will use our map to plan your next adventure. If your in the dirt or on the rode you might find some new and exciting places to ride. We also strive to help you fix any bike related problem that you might have. Please check out our Fix it page at the top of our web site. If you have any questions please send us an email and we'll get right back to you.</p> 
+
+<img src="bike02.jpeg" alt="Icon" class="picture01" style="width:700px;height:500px;">
+
+<?php
+// In order to be dtransparent Alot of this code has been sampled from:https://www.w3schools.com/php/php_form_validation.asp
+
+
+//Define var
+$emailErr = $commentErr = "";
+$email = $comment = "";
+
+if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+
+if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+//if the input meets the req, the input is assigned to the var
+  function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+?>
+
+<h3>Please enter you email and submit any questions that you might have</h3>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<p><span class="error">* required field</span></p>
+
+	E-mail: <input type="text" name="email">
+  	<span class="error">* <?php echo $emailErr;?></span>
+  	<br><br>
+  	Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  	<span class="error">* <?php echo $commentErr;?></span>
+  	<br><br>
+  	<input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+if ($email != "")
+	echo "<h2>Input:</h2>";
+	echo $email;
+	echo "<br>";
+	echo $comment;
+	echo "<br>";
+?>
+
+
+</body>
+</html>
---
+++ pageTwo.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>News</title>
+	<link rel = "stylesheet"
+   	type = "text/css"
+   	href = "myStyle.css" />
+</head>
+<body>
+
+<h1>Fixing your Bike</h1>
+<div class="navbar">
+  <a href="main.php">Home</a>
+  <a href="pageTwo.php">News</a>
+  <div class="dropdown">
+    <button class="dropbtn">Assignments 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Assignment 1</a>
+      <a href="#">Assignment 2</a>
+      <a href="#">Assignment 3</a>
+    </div>
+  </div> 
+</div>
+
+<embed width="420" height="315"
+data="https://www.youtube.com/watch?v=enG_XbSDrqg">
+</embed>
+
+<br>
+<iframe width="420" height="315"
+src="https://www.youtube.com/watch?v=Bbk5RcH0bbQ">
+</iframe> 
+
+</body>
+</html> 
